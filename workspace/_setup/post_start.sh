8cc7f5c2b1c09bc0a11ea35de4d2abbe3cd6c072
Michael Akridge <michael.akridge@noaa.gov>
2023-10-27 11:10:23 -1000

    Update post_start.sh

--- workspace/_setup/post_start.sh
+++ workspace/_setup/post_start.sh
@@ -4,9 +4,7 @@
 message="# Welcome to your RStudio Codespace!
 - This readme should open up at same time as the VS code container. 
 - If there are any persistent data changes, you might want to back up the data
-
-# Access RStudio
-Your application is available at this URL:
+- Your application is available at this URL:
 
 ## RStudio URL: https://$CODESPACE_NAME-8787.app.github.dev
 "
@@ -15,7 +13,18 @@ echo "$message"
 
 # Append the message to README.md
 echo "$message" >> /home/rstudio/workspace/README.md
+# Detect the Linux distribution
+DISTRO=$(cat /etc/*-release | grep ^ID | tr -d '"' | cut -d= -f2)
 
-sleep 10
-
+# Install dependencies based on the detected distribution
+if [ "$DISTRO" == "gentoo" ]; then
+    sudo emerge --ask sci-libs/geos
+elif [ "$DISTRO" == "centos" ]; then
+    sudo yum install geos geos-devel
+elif [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "ubuntu" ]; then
+    sudo apt update
+    sudo apt install libgeos-dev
+else
+    echo "Unsupported distribution"
+fi
 
