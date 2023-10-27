bdbdae7ac22616360fe041135556d5694229de56
Michael Akridge <michael.akridge@noaa.gov>
2023-10-27 09:38:45 -1000

    v1

---
+++ post_start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Construct the message
+message="# Welcome to your RStudio Codespace!
+- This readme should open up at same time as the VS code container. 
+- If there are any persistent data changes, you might want to back up the 
+
+# Access RStudio
+Your application is available at this URL:
+
+## RStudio URL: https://$CODESPACE_NAME-8787.app.github.dev
+
+## Notes:
+If its not started yet, you can view the progress with these commands in the terminal: 
+\`\`\`
+### view containers
+
+docker ps
+
+### view container logs
+docker logs rstudio
+\`\`\`
+"
+# Echo the message to the terminal
+echo "$message"
+
+# Append the message to README.md
+echo "$message" >> /workspaces/ncei_eds/README.md
+
+sleep 10
+
+
