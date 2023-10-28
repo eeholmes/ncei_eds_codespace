daa1f5776f4237035a042737558baeab6bb9eb61
Michael Akridge <michael.akridge@noaa.gov>
2023-10-27 14:24:37 -1000

    Update post_start.sh

--- workspace/_setup/post_start.sh
+++ workspace/_setup/post_start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Construct the message
-message="# Welcome to your RStudio Codespace!
+message="# Welcome to your ESD RStudio Codespace!
 - This readme should open up at same time as the VS code container. 
-- If there are any persistent data changes, you might want to back up the data
-- Various project files and scripts need thier paths updated. 
-- Review slides for more info on howto use
+- If there are any persistent data changes, you might want to backup/export the data 
+- Various project files and scripts will need thier paths updated. 
+- Review slides for more info on how to use
 
 # Access RStudio
-Your application is available at this URL:
+- ## RStudio URL: https://$CODESPACE_NAME-8787.app.github.dev
 
-## RStudio URL: https://$CODESPACE_NAME-8787.app.github.dev
+## Slides:
+- ## https://$CODESPACE_NAME-8787.app.github.dev/files/workspace/slides/CW_Verstaen_EDS.pdf
+- ## https://$CODESPACE_NAME-8787.app.github.dev/files/workspace/slides/ESD_workshop_02.pdf
 
 "
 # Echo the message to the terminal
